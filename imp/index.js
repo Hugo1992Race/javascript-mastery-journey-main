846bfed6ed0b48be03cc174f7efce155b1fd6c26
Hugo1992Race <vladivladovic@gmail.com>
2025-02-10 15:43:31 +0300

    Delete imp directory

--- imp/index.js
+++
@@ -1,5 +0,0 @@
-const readlineSync = require('readline-sync');
- 
-// Wait for user's response.
-let userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
