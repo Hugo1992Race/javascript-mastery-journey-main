60bf4985fd9b9e8fe806f3c1310b004bab0c95c7
Hugo1992Race <vladivladovic@gmail.com>
2025-02-10 15:48:48 +0300

    Delete 021-dom-manipulation directory

--- 021-dom-manipulation/00-script.js
+++
@@ -1,8 +0,0 @@
-function newNameBtnClick() {
-    const tb = document.getElementById("textBox");
-    tb.value = `Привет, ${tb.value}!`;
-}
-
-const btn = document.getElementById("btnAction");
-
-btn.addEventListener("click", newNameBtnClick);
