c951f96b81d6dfcb53eacb6d46c09c64831c5fa0
Hugo1992Race <vladivladovic@gmail.com>
2025-02-10 15:44:22 +0300

    Delete 008-conditional directory

--- 008-conditional/ex-02.js
+++
@@ -1,9 +0,0 @@
-let a = 111, b = 21, c = 31;
-
-if (a > b && a > c) {
-    console.log(`max = ${a}`);
-} else if (b > c) {
-    console.log(`max = ${b}`);
-} else {
-    console.log(`max = ${c}`);
-}
--- 008-conditional/main.js
+++
@@ -1,9 +0,0 @@
-let a = 1, b = 2, c = 3
-
-if (a > b && a > c) {
-    console.log(`max = ${a}`)
-} else if (b > c) {
-    console.log(`max = ${b}`)
-} else {
-    console.log(`max = ${c}`)
-}
